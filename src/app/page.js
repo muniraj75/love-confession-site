a328cb2dbefe5474e937f2461ea861ec7173164e
muniraj75 <muniraj7575@gmail.com>
2026-03-12 21:52:21 +0530

    Create page.js

---
+++ src/app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{textAlign:"center",marginTop:"120px"}}>
+      <h1>I Love You ❤️</h1>
+      <p>This website is only for you.</p>
+    </div>
+  );
+}
